0fb814c9e8128159c8a49d4ca915cb4c2d2976c5
Josa Ferreira <josenildo.junior9321@gmail.com>
2023-07-14 20:36:27 -0300

    JUMENTO officialized

--- src/JUMENTO.jl
+++ src/JUMENTO.jl
@@ -7,15 +7,7 @@ import Combinatorics: permutations
 include("structs.jl")
 include("dominance_relations.jl")
 include("augmecon.jl")
-# include("weighted_sum.jl")
 
 export augmecon
 
 end
-
-#===================
-= When using, if I want to save the solution, I need to declare:
- * Variable <: VariablesJuMP
- * init_augmecon_variables(instance)
- * register_variables!(solution, augmecon_model, instance)
-=#
