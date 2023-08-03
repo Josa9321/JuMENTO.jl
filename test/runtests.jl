b536675e6982e72ecb46851619fb45a6a09f2ae6
Josa Ferreira <josenildo.junior9321@gmail.com>
2023-08-03 11:17:32 -0300

    testing kp instances

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,5 @@ test_instances(files_names=["2kp50.xlsx", "2kp100.xlsx", "2kp250.xlsx"], compare
     solve_instance = solve_kp_instance, folder = "mokp//instances//"
 )
 test_instances(files_names=["3kp40.xlsx", "3kp50.xlsx"],
-    solve_instance = solve_kp_instance, folder = "test//mokp//instances//", 
+    solve_instance = solve_kp_instance, folder = "mokp//instances//", 
 )
