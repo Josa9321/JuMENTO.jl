3e805f405eadf3ba9acfbd01b5e409e8b79933ff
Josa9321 <josenildo.junior9321@gmail.com>
2023-08-09 17:08:24 -0300

    names changes on test .jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,10 @@ include("optimization_models.jl")
 include("tests.jl")
 include("mokp//load.jl")
 
-test_instances(files_names=["2kp50.xlsx", "2kp100.xlsx", "2kp250.xlsx"], compare_payoff = true,
-    solve_instance = solve_kp_instance, folder = "mokp//instances//"
-)
-test_instances(files_names=["3kp40.xlsx", "3kp50.xlsx"],
-    solve_instance = solve_kp_instance, folder = "mokp//instances//", 
-)
+# test_instances(files_names=["2kp50.xlsx", "2kp100.xlsx", "2kp250.xlsx"], compare_payoff = true,
+#     solve_instance = solve_kp_instance, folder = "mokp//instances//"
+# )
+# test_instances(files_names=["3kp40.xlsx", "3kp50.xlsx"],
+#     solve_instance = solve_kp_instance, folder = "mokp//instances//", 
+# )
+
