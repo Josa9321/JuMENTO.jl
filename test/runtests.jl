95b18d4869af26eeec5ac56e4c6232d87cabf116
Gabriel <gabrielcorreia654@gmail.com>
2025-07-23 15:27:21 -0300

    Others adjustments

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,22 +10,22 @@ test_simple_problems(
     simple_biobjective_table(),
     is_augmecon_2 = false
 )
-# test_simple_problems(
-#     simple_biobjective_problem, 
-#     simple_biobjective_frontier(), 
-#     simple_biobjective_table(),
-#     is_augmecon_2 = true
-# )
+test_simple_problems(
+    simple_biobjective_problem, 
+    simple_biobjective_frontier(), 
+    simple_biobjective_table(),
+    is_augmecon_2 = true
+)
 
-# test_simple_problems(
-#     simple_triobjective_problem, 
-#     simple_triobjective_frontier(), 
-#     simple_triobjective_table(),
-#     is_augmecon_2 = false
-# )
-# test_simple_problems(
-#     simple_triobjective_problem, 
-#     simple_triobjective_frontier(), 
-#     simple_triobjective_table(),
-#     is_augmecon_2 = true
-# )
+test_simple_problems(
+    simple_triobjective_problem, 
+    simple_triobjective_frontier(), 
+    simple_triobjective_table(),
+    is_augmecon_2 = false
+)
+test_simple_problems(
+    simple_triobjective_problem, 
+    simple_triobjective_frontier(), 
+    simple_triobjective_table(),
+    is_augmecon_2 = true
+)
