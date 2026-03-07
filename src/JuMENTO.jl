5b072288e558543b4e880bdce3d95f68f0aac8b1
Josa9321 <josenildo.junior9321@gmail.com>
2026-03-07 07:58:46 -0300

    style: reorganize directory structure for better modularity

--- src/JuMENTO.jl
+++ src/JuMENTO.jl
@@ -5,23 +5,23 @@ import MathOptInterface as MOI
 
 export generate_pareto, normalize_frontier, Metrics, MultiPlots, SolutionJuMP, ReportAUG, augmecon, nsga2
 
-include("structs.jl")
-include("dominance_relations.jl")
-include("normalize.jl")
-include("options.jl")
+include("utils/structs.jl")
+include("utils/dominance_relations.jl")
+include("utils/normalize.jl")
 
 # Colocar em um módulo os métodos da família AUGMECON
 # Colocar em um módulo as heurísticas
 # Reaproveitar funções declaradas para o AUGMECON para serem usadas nas heurísticas
 
-include("augmecon.jl")
+include("MethodAUGMECON/options.jl")
+include("MethodAUGMECON/augmecon.jl")
 
-include("nsga2_options.jl")
-include("nsga2.jl")
+include("MethodNSGA/nsga2_options.jl")
+include("MethodNSGA/nsga2.jl")
 
 include("Metrics/Metrics.jl")
 include("Plots/MultiPlots.jl")
 
-include("utils.jl")
+include("utils/print_msgs.jl")
 
 end
